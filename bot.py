b348ade2a521cf330ce0741ebcd692ef44d91f0e
kryvenchukuberem-commits <kryvenchuk.uberem@gmail.com>
2026-02-20 11:06:50 +0200

    Fix token string format in bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот працює ✅")
 
 def main():
-    application = Application.builder().token(8460126618:AAGXWc7PmSDn5oiW5sKDXb7EogVqQ-P9NJg).build()
+    application = Application.builder().token("8460126618:AAGXWc7PmSDn5oiW5sKDXb7EogVqQ-P9NJg").build()
     application.add_handler(CommandHandler("start", start))
     application.run_polling()
 
