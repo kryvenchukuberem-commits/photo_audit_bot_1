cc7a5dba59dbacba91904ec73ed0f553ae0eccbf
kryvenchukuberem-commits <kryvenchuk.uberem@gmail.com>
2026-02-20 12:58:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def get_current_month():
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Вітаю! Надішліть одне актуальне фото.\n"
+        "Вітаю! Надішліть два актуальне фото.\n"
         "Повторні або старі фото не приймаються."
     )
 
@@ -34,17 +34,6 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
     username = update.message.from_user.username
     current_month = get_current_month()
-
-    # Перевірка: чи вже здавав фото цього місяця
-    cursor.execute("SELECT * FROM photos WHERE user_id=? AND month=?", (user_id, current_month))
-    if cursor.fetchone():
-        await update.message.reply_text("❌ Ви вже здали фото цього місяця.")
-        return
-
-    photo = update.message.photo[-1]
-    file = await context.bot.get_file(photo.file_id)
-    file_path = f"{photo.file_id}.jpg"
-    await file.download_to_drive(file_path)
 
     # Хеш фото
     with open(file_path, "rb") as f:
