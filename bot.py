a529b9dffe200fbfb62780738a6bc41c6c5a841d
kryvenchukuberem-commits <kryvenchuk.uberem@gmail.com>
2026-02-20 13:31:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,12 +49,6 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
         return
         
-    # Перевірка: чи вже здавав фото цього місяця
-    cursor.execute("SELECT * FROM photos WHERE user_id=? AND month=?", (user_id, current_month))
-    if cursor.fetchone():
-        await update.message.reply_text("❌ Ви вже здали фото цього місяця.")
-        return
-        
      photo = update.message.photo[-1]
     file = await context.bot.get_file(photo.file_id)
     file_path = f"{photo.file_id}.jpg"
