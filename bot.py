58a656fcf86172ab114349fca15ade074e554d8b
kryvenchukuberem-commits <kryvenchuk.uberem@gmail.com>
2026-02-20 11:04:42 +0200

    Update bot.py to use environment variable for token
    Replaced hardcoded token with environment variable placeholder.

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8460126618:AAGXWc7PmSDn5oiW5sKDXb7EogVqQ-P9NJg")
+TOKEN = os.getenv("YOUR_TOKEN_HERE")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот працює ✅")
