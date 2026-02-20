e40756ed100b33834c9ebda06b203be32eccaeb1
kryvenchukuberem-commits <kryvenchuk.uberem@gmail.com>
2026-02-20 13:27:27 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,6 +35,12 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     username = update.message.from_user.username
     current_month = get_current_month()
 
+    # Перевірка: чи вже здавав фото цього місяця
+    cursor.execute("SELECT * FROM photos WHERE user_id=? AND month=?", (user_id, current_month))
+    if cursor.fetchone():
+        await update.message.reply_text("❌ Ви вже здали фото цього місяця.")
+        return
+        
      photo = update.message.photo[-1]
     file = await context.bot.get_file(photo.file_id)
     file_path = f"{photo.file_id}.jpg"
@@ -49,6 +55,20 @@ async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if cursor.fetchone():
         await update.message.reply_text("❌ Це фото вже надсилалось раніше.")
         os.remove(file_path)
+        return
+
+    # Перевірка ліміту 2 фото на місяць
+    cursor.execute(
+        "SELECT COUNT(*) FROM photos WHERE user_id=? AND month=?",
+        (user_id, current_month)
+    )
+    count = cursor.fetchone()[0]
+
+    if count >= 2:
+        await update.message.reply_text(
+            "⚠️ Ви вже надіслали 2 фото цього місяця.\n"
+            "Спробуйте знову наступного місяця."
+        )
         return
 
     # Зберігаємо
