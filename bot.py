5cb8887532d995b98969b604acd3cfe037b155a4
kryvenchukuberem-commits <kryvenchuk.uberem@gmail.com>
2026-02-20 11:02:46 +0200

    Fix token retrieval method in bot.py
    Updated the way the bot token is retrieved from environment variables.

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv(8460126618:AAGXWc7PmSDn5oiW5sKDXb7EogVqQ-P9NJg)
+TOKEN = os.getenv("8460126618:AAGXWc7PmSDn5oiW5sKDXb7EogVqQ-P9NJg")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот працює ✅")
 
 def main():
-    application = Application.builder().token(TOKEN).build()
+    application = Application.builder().token(8460126618:AAGXWc7PmSDn5oiW5sKDXb7EogVqQ-P9NJg).build()
     application.add_handler(CommandHandler("start", start))
     application.run_polling()
 
